3142186f37d55c6ec5d4f137ff5c50739c8cb76d
wperron <william.perron@live.ca>
2019-02-25 19:15:16 -0500

    added object-oriented version -- first pass

--- pyfizzbuzz/__init__.py
+++ pyfizzbuzz/__init__.py
@@ -44,3 +44,17 @@ def higher_order(n):
 
     return fizz(buzz(identity))(n)
 
+
+class Fizz:
+    def __call__(self, n):
+        return "Fizz" * (n % 3 == 0) or ''
+
+class Buzz:
+    def __call__(self, n):
+        return "Buzz" * (n % 5 == 0) or ''
+
+
+def object_oriented(n):
+    fizz = Fizz()
+    buzz = Buzz()
+    return fizz(n) + buzz(n) or n
